ab814151b25d6c904ccb38190724dc7805a26e39
jramirez <jramirez@chicagotechacademy.org>
2013-12-17 15:00:29 -0600

    creating web app

---
+++ blog_blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogBlog::Application.config.secret_token = '447395fd8bbd2979eae1aa9cb643031974a53535edafd247c4ad06b89e3650f746b0d875355f88f8285de5a9472d6663bc58d2f171cbda81b090a4128471113e'
